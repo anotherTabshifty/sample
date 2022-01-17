fd9d37804fbed60918afa8aadf027724893531e5
mfangit <2639972841@qq.com>
2022-01-17 18:22:15 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const { src, dest, watch } = require('gulp')
+const { parallel,src, dest, watch } = require('gulp')
 const { pug2html } = require(`./task/pugConvert`)
 const browserSync = require('browser-sync').create()
 const { buildSass } = require('./task/sassConvert')
@@ -8,7 +8,20 @@ function transferJs() {
     .pipe(dest('./dist/preview/js'))
 }
 
+function transferIconfont() {
+  return src(['./src/iconfont/**/*', './src/iconfont/*'])
+  .pipe(dest('./dist/preview/iconfont'))
+}
 
+function transferFonts() {
+  return src('./src/fonts/*')
+  .pipe(dest('./dist/preview/fonts'))
+}
+
+function transferImages() {
+  return src('./src/images/*')
+  .pipe(dest('./dist/preview/images'))
+}
 
 exports.default = function() {
   browserSync.init({
@@ -18,7 +31,10 @@ exports.default = function() {
 
   watch(['./src/pug/*.pug','./src/pug/**/*.pug'], pug2html)
   watch(['./src/js/*.js','./src/js/**/*.js'], transferJs)
-  watch('./src/css/style.scss', buildSass)
+  watch(['./src/iconfont/**/*', './src/iconfont/*'], transferIconfont)
+  watch('./src/images/*', transferImages)
+  watch('./src/fonts/*', transferFonts)
+  watch(['./src/css/style.scss', `./src/css/**/*.scss`], buildSass)
   watch(['./dist/preview/*', './dist/preview/css/*', './dist/preview/js/*'], function(cb) {
     browserSync.reload()
     cb()
--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,28 @@
-window.onload = function () {
-    new WOW({
-        animateClass: "animate__animated"
-    }).init()
-}
+$(document).ready(function() {
+    $('.swiper').slick({
+        arrows: false,
+        dots: true,
+    })
+    // add event
+    var flag = false
+    var $expandMenu = $('.expand-menu')
+    var $responsiveMenu = $('.responsive-menus')
+    function removeActive() {
+        $expandMenu.removeClass('active')
+        $responsiveMenu.removeClass('active')
+    }
+    $expandMenu.click(function() {
+        if (!flag) {
+            $expandMenu.addClass('active')
+            $responsiveMenu.addClass('active')
+        } else {
+            removeActive()
+        }
+        flag = !flag
+    })
+
+    $responsiveMenu.click(function(){
+        removeActive()
+        flag = false
+    })
+})
