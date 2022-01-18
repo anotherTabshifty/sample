e0a478eb680d0afdaab8eaf65fa2630a32f3cb47
mfangit <2639972841@qq.com>
2022-01-18 13:19:27 +0800

    update

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,21 @@
 $(document).ready(function() {
-    $('.swiper').slick({
-        arrows: false,
-        dots: true,
-    })
-    // add event
+    
     var flag = false
     var $expandMenu = $('.expand-menu')
     var $responsiveMenu = $('.responsive-menus')
+
     function removeActive() {
         $expandMenu.removeClass('active')
         $responsiveMenu.removeClass('active')
     }
-    $expandMenu.click(function() {
+
+    $('.swiper').slick({
+        arrows: false,
+        dots: true,
+    })
+
+    // add event
+    $expandMenu.click(function(e) {
         if (!flag) {
             $expandMenu.addClass('active')
             $responsiveMenu.addClass('active')
@@ -19,10 +23,26 @@ $(document).ready(function() {
             removeActive()
         }
         flag = !flag
+        e.stopPropagation()
+
     })
 
-    $responsiveMenu.click(function(){
+    $responsiveMenu.click(function(e){
         removeActive()
         flag = false
+        e.stopPropagation()
+    })
+
+    $(window).resize(function(){
+        var _window = this
+        if(!flag) return
+        if (_window.innerWidth > 1024) {
+            flag = false
+            removeActive()
+        }
+    })
+
+    new WOW().init({
+        animateClass: "animate__animated" 
     })
 })
